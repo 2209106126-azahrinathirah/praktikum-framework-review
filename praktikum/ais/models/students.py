2ffd858cc001c699a30913f129f3a68a0dbe5715
2209106126-azahrinathirah <azahrinathiraha051@gmail.com>
2024-10-01 09:06:30 +0800

    review 4

--- praktikum/ais/models/students.py
+++ praktikum/ais/models/students.py
@@ -7,11 +7,11 @@ class Students(models.Model):
     MinValueValidator(1), # NIM bernilai Positif
     MaxValueValidator(10**10-1) # NIM Maksimal 10 digit
 ])
-name = models.CharField(max_length=255)
-email = models.EmailField(max_length=255, unique=True)
-phone_number = models.CharField(max_length=13, unique=True)
-year = models.IntegerField() # Tahun angkatan atau tahun masuk
-teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE) # Relasi ke tabel Teachers
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=13, unique=True)
+    year = models.IntegerField() # Tahun angkatan atau tahun masuk
+    teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE) # Relasi ke tabel Teachers
 
 def __str__(self):
     return self.name
